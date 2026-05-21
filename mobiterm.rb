14edcb825ebdb804e7b0a946c5310e06061e0b00
wiltra-dev <wiltra.dev@gmail.com>
2026-05-21 16:18:21 +0000

    update mobiterm to v0.2.0

--- mobiterm.rb
+++ mobiterm.rb
@@ -4,7 +4,7 @@
 class Mobiterm < Formula
   desc "A mobile-first web terminal for remote access from any device."
   homepage "https://github.com/lampstand-labs/mobiterm"
-  version "0.1.0"
+  version "0.2.0"
 
   livecheck do
     url :stable
@@ -13,23 +13,23 @@ class Mobiterm < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.1.0/mobiterm-darwin-x64.tar.gz"
-      sha256 "4722c1516f52d8aef2421e532cfe4b8c1f588d8da34a6ce79473eb93d107128a"
+      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.2.0/mobiterm-darwin-x64.tar.gz"
+      sha256 "9a87bc69a8b4eb24981b25606bc7239366eb6be2796425b0f7776ca3768ad511"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.1.0/mobiterm-darwin-arm64.tar.gz"
-      sha256 "80a1aa350b8cff8c23117ec3ceadb3fe17bbd2520ca61981ec960f787b9c85d1"
+      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.2.0/mobiterm-darwin-arm64.tar.gz"
+      sha256 "074f9138ee1e817e7d190a9876ce7abd1f0f3579781d3a7da523326b8e6693bc"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.1.0/mobiterm-linux-x64-gnu.tar.gz"
-      sha256 "9502f89d023b23da130c85405a3a204ca41c8574dccd0a85ce9f96e7e605d0b8"
+      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.2.0/mobiterm-linux-x64-gnu.tar.gz"
+      sha256 "03b2681f4ae49e865efb76616443139ac480de438b0133b649dfc7ecfb6e2b4c"
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.1.0/mobiterm-linux-arm64-gnu.tar.gz"
-      sha256 "00cfb7083d0b882acd1c90664113a5e2b6c54d09122e304746f318a5abcdcc2f"
+      url "https://github.com/lampstand-labs/mobiterm/releases/download/v0.2.0/mobiterm-linux-arm64-gnu.tar.gz"
+      sha256 "bc89664d7210b9d842e5e6cb2132f173b37b227833f041090d07c60e5f365dda"
     end
   end
 
